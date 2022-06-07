d1d55da2e1d04feef3a538b8267530651546f471
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-07 11:42:15 -0700

    completed final tables exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,11 @@
 USE codeup_test_db;
-DROP TABLE IF EXISTS album;
+DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
-    name VARCHAR (100) NOT NUll,
+    name VARCHAR(100) NOT NUll,
     release_date INT UNSIGNED,
     sales FLOAT,
-    genre VARCHAR (100),
+    genre VARCHAR(200),
     PRIMARY KEY (id)
-)
+);
