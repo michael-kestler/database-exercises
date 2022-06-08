31b6d95bb20851d58161c9a1954f4c73c04bd17a
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-08 15:08:50 -0700

    completed sixth task of functions exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -7,3 +7,11 @@ SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'E%%E'
 
 SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%Q%' AND last_name
     NOT LIKE '%QU%';
+
+
+SELECT DISTINCT last_name, COUNT(last_name)
+FROM employees
+WHERE last_name LIKE '%Q%' AND last_name
+    NOT LIKE '%QU%'
+GROUP BY last_name
+ORDER BY COUNT(*) DESC;
