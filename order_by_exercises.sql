0a3d4360d6fc4e96fcf6e37aac6791e101c5575c
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-08 12:11:51 -0700

    sorted e and ee query by employee number and then sorted again in descending order. completed order by exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -19,11 +19,11 @@ SELECT * FROM employees WHERE gender = 'm' AND (
         first_name IN ('Irena', 'Maya', 'Vidya')
     );
 
-SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
 
 SELECT * FROM employees WHERE last_name LIKE '%Q%';
 
-SELECT * FROM employees WHERE last_name LIKE 'E%%E';
+SELECT * FROM employees WHERE last_name LIKE 'E%%E' ORDER BY emp_no DESC;
 
 SELECT * FROM employees WHERE last_name LIKE '%Q%' AND last_name
     NOT LIKE '%QU%';
