42970d8226eb7d24efa0da5eba4398974b3d02e7
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-08 10:25:54 -0700

    added AND condition to previous query to find all employees with those names and who is also a male. obtained 441 results

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,6 +1,13 @@
 USE employees;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
 SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya'
 OR first_name = 'Maya';
+
+SELECT * FROM employees WHERE gender = 'm' AND (
+    first_name IN ('Irena', 'Maya', 'Vidya')
+    );
 
 SELECT * FROM employees WHERE last_name LIKE 'E%';
 
