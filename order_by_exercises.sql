11178a55663b93d5e2ef3b6e9c61374d4f25f0dd
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-08 11:52:17 -0700

    created queries to search through employees names and display by name order and then first and last name order

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,6 +2,13 @@ USE employees;
 
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY  first_name;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY  first_name, last_name;
+
+
 SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya'
                            OR first_name = 'Maya';
 
