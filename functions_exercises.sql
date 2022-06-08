7df28afc3ef673b3f5e8e0a49c2054e19e8d03b8
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-08 14:21:44 -0700

    completed fifth task of functions exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -33,7 +33,7 @@ SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E
 SELECT * FROM employees WHERE last_name LIKE '%Q%' AND last_name
     NOT LIKE '%QU%';
 
-SELECT * FROM employees WHERE birth_date LIKE '%12-25';
+SELECT * FROM employees WHERE birth_date LIKE '%12-25' ORDER BY birth_date DESC;
 
 SELECT *
 FROM employees
@@ -43,6 +43,7 @@ AND day(birth_date) = 25;
 
 SELECT *
 FROM employees
-WHERE year(birth_date) BETWEEN 1990 AND 1999
+WHERE year(hire_date) BETWEEN 1990 AND 1999
   AND month(birth_date) = 12
-  AND day(birth_date) = 25;
+  AND day(birth_date) = 25
+ORDER BY hire_date DESC;
