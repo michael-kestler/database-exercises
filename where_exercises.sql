993026ca4ffc69cc75d0d82e03a8c3100630aacb
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-08 10:13:52 -0700

    changed employee first name query to use OR instead of IN

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,9 @@
 USE employees;
-SELECT * FROM employees WHERE first_name
-    IN ('Irena', 'Vidya', 'Lipner', 'Maya' );
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya'
+OR first_name = 'Maya';
 
 SELECT * FROM employees WHERE last_name LIKE 'E%';
 
 SELECT * FROM employees WHERE last_name LIKE '%Q%';
+
+
