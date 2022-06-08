f3177acaaa290621cf1f611482227b254a254f61
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-07 22:40:19 -0700

    refactored third exercise on basic statements

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,7 +1,14 @@
 USE codeup_test_db;
-SELECT 'Album Name' AS 'All albums in the table: ', name, release_date, sales FROM albums;
+SELECT 'Album Name' AS 'All albums in the table', name, release_date, sales FROM albums;
 UPDATE albums
 SET sales=sales*10;
 SELECT 'Sales' AS 'Sales growth', sales FROM albums;
-SELECT 'Album Name' AS 'All albums released before 1980: ', name, release_date FROM albums WHERE release_date < '1980';
+SELECT 'Album Name' AS 'All albums released before 1980', name, release_date FROM albums WHERE release_date < '1980';
+UPDATE albums
+SET release_date=release_date-100
+WHERE release_date < 1980;
+SELECT 'Album Name' AS 'All albums released before 1880', name, release_date FROM albums WHERE release_date < '1880';
 SELECT 'Album Name' AS 'Albums by Michael Jackson', name FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums
+SET artist='Peter Jackson'
+WHERE artist = 'Michael Jackson';
