bf5c15501348acfca5321815160087bcd168d2c3
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-09 07:19:40 -0700

    completed third task of aliases exercise - return 10 employees in a result named full_name in the format of last name, first name

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,1 +1,13 @@
 USE employees;
+
+SELECT CONCAT(last_name, ', ', first_name) AS full_name
+FROM employees
+LIMIT 10;
+
+
+
+
+
+
+#        birth_date FROM employees
+# WHERE birth_date LIKE '%04-01' AND birth_date NOT LIKE '195%';
