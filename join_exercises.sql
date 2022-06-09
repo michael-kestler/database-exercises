25315d1bc12e7fcab05d53d589a24c483e526225
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-09 14:21:20 -0700

    completed task to get current salary of all current department managers

--- join_exercises.sql
+++ join_exercises.sql
@@ -17,3 +17,19 @@ FROM employees as e
               ON d.dept_no = de.dept_no
 WHERE de.to_date = '9999-01-01' AND gender = 'f';
 ;
+
+
+
+
+
+
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name)
+    AS 'Department Manager', s.salary AS 'Salary'
+FROM employees as e
+         JOIN dept_manager as de
+              ON de.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+         JOIN salaries s on e.emp_no = s.emp_no
+WHERE de.to_date = '9999-01-01' AND s.to_date = '9999-01-01';
+;
